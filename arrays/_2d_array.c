0d0147093a382060637269ebfecfed6ad41c8453
devanand072001 <devanand.cs19@bitsathy.ac.in>
2021-10-24 16:03:14 +0530

    c practice

--- arrays/_2d_array.c
+++ arrays/_2d_array.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-void main(){
-    int a [3][5] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130};
-    printf("%p\n",a);
-    printf("%p\n",a + 1);
-    printf("%p\n",*a);
+void main()
+{
+    int a[3][5] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130};
+    printf("%p\n", a);
+    printf("%p\n", &*(a[2]) );
+    printf("%p\n", *a + 1);
     printf("%d\n", **a);
 }
